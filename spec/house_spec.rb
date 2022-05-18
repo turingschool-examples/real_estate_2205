49b13e1978191e0df731483ff3d7beb55de60d8e
Blake Saylor <blakesaylor@gmail.com>
2022-05-18 11:20:54 -0600

    Create test for add_room method

--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -22,5 +22,15 @@ RSpec.describe House do
       house = House.new("$400000", "123 sugar lane")
       expect(house.rooms).to eq []
     end
+
+    it "can add rooms" do
+      house = House.new("$400000", "123 sugar lane")
+      expect(house.rooms).to eq []
+      room_1 = Room.new(:bedroom, 10, '13')
+      room_2 = Room.new(:bedroom, 11, '15')
+      house.add_room(room_1)
+      house.add_room(room_2)
+      expect(house.rooms).to eq [room_1, room_2]
+    end
   end
 end
