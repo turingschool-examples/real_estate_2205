8f1407cd8a72606e2c9be92983665f64c0f266d0
Ethan Nguyen <etnguyen16@gmail.com>
2022-05-18 17:45:40 -0600

    add room and room spec

--- lib/room.rb
+++ lib/room.rb
@@ -1,8 +1,41 @@
+require'pry'
+
 class Room
+  attr_reader :category, :length, :width, :painted
 
-  def initialize
-
+  def initialize(category, length, width)
+    @category = category
+    @length = length.to_i
+    @width = width.to_i
+    @painted = false
   end
+
+
+
+
+  def area
+    area = @length * @width
+  end
+
+
+
+
+  def is_painted?
+    if @painted = true
+      return true
+    elsif
+    return false
+    end
+  end
+
+
+
+
+  def paint
+    @painted = true
+  end
+
+
 
 
 end
