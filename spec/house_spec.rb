0c2e26b5e41daea8d8ac0c00aede6d86438afc8a
Blake Saylor <blakesaylor@gmail.com>
2022-05-18 11:14:51 -0600

    Add test for existence of house

---
+++ spec/house_spec.rb
@@ -0,0 +1,11 @@
+require './lib/room'
+require './lib/house'
+
+RSpec.describe House do
+  describe "Iteration 2" do
+    it "exists" do
+      house = House.new("$400000", "123 sugar lane")
+      expect(house).to be_a House
+    end
+  end
+end
