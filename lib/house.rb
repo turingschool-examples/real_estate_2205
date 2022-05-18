c88e8bb7392cefb9d8b16e63809af741f8ad2008
Blake Saylor <blakesaylor@gmail.com>
2022-05-18 12:36:38 -0600

    Update rooms_by_category to pass test

--- lib/house.rb
+++ lib/house.rb
@@ -45,4 +45,32 @@ class House
     return (@price.to_f / area.to_f).round(2)
   end
 
+  def rooms_sorted_by_area
+
+  end
+
+  def rooms_by_category
+    rooms_by_category = Hash.new
+    room_categories = []
+    @rooms.each do |room|
+      room_categories << room.category
+    end
+    room_categories.uniq!
+    room_categories.each do |category|
+      rooms_by_category[category] = rooms_from_category(category)
+    end
+    return rooms_by_category
+  end
+
 end
+
+house = House.new("$400000", "123 sugar lane")
+room_1 = Room.new(:bedroom, 10, '13') #130
+room_2 = Room.new(:bedroom, 11, '15') #165
+room_3 = Room.new(:living_room, 25, '15') #375
+room_4 = Room.new(:basement, 30, '41') #1230
+house.add_room(room_1)
+house.add_room(room_2)
+house.add_room(room_3)
+house.add_room(room_4)
+house.rooms_by_category
