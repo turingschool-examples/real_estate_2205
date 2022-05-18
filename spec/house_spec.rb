4bcf0f0d7b5884c3cd413054062e2cb53d98e16d
Blake Saylor <blakesaylor@gmail.com>
2022-05-18 11:18:01 -0600

    Add tests for price and address

--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -7,5 +7,15 @@ RSpec.describe House do
       house = House.new("$400000", "123 sugar lane")
       expect(house).to be_a House
     end
+
+    it "has a price" do
+      house = House.new("$400000", "123 sugar lane")
+      expect(house.price).to eq 400000
+    end
+
+    it "has an address" do
+      house = House.new("$400000", "123 sugar lane")
+      expect(house.address).to eq "123 sugar lane"
+    end
   end
 end
