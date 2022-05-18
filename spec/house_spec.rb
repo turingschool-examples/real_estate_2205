2a7128344e92b54dc6a4000c87a3330e4da8125c
Blake Saylor <blakesaylor@gmail.com>
2022-05-18 11:31:13 -0600

    Create test for area of house

--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -54,5 +54,18 @@ RSpec.describe House do
       expect(house.rooms_from_category(:living_room)).to eq [room_3]
       expect(house.rooms_from_category(:patio)).to eq []
     end
+
+    it "can calculate area based on areas of rooms" do
+      house = House.new("$400000", "123 sugar lane")
+      room_1 = Room.new(:bedroom, 10, '13')
+      room_2 = Room.new(:bedroom, 11, '15')
+      room_3 = Room.new(:living_room, 25, '15')
+      room_4 = Room.new(:basement, 30, '41')
+      house.add_room(room_1)
+      house.add_room(room_2)
+      house.add_room(room_3)
+      house.add_room(room_4)
+      expect(house.area).to eq 1900
+    end
   end
 end
