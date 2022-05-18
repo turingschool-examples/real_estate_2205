6400b2f6ef1d515d55ca8b28a7b61e83956469af
Blake Saylor <blakesaylor@gmail.com>
2022-05-18 11:27:32 -0600

    Add test for rooms_from_category

--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -39,5 +39,20 @@ RSpec.describe House do
       expect(house_1.above_market_average?).to eq false
       expect(house_2.above_market_average?).to eq true
     end
+
+    it "can list rooms from a category" do
+      house = House.new("$400000", "123 sugar lane")
+      room_1 = Room.new(:bedroom, 10, '13')
+      room_2 = Room.new(:bedroom, 11, '15')
+      room_3 = Room.new(:living_room, 25, '15')
+      room_4 = Room.new(:basement, 30, '41')
+      house.add_room(room_1)
+      house.add_room(room_2)
+      house.add_room(room_3)
+      house.add_room(room_4)
+      expect(house.rooms_from_category(:bedroom)).to eq [room_1, room_2]
+      expect(house.rooms_from_category(:living_room)).to eq [room_3]
+      expect(house.rooms_from_category(:patio)).to eq []
+    end
   end
 end
