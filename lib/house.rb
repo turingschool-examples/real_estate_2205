7f483c09528bf5d7548afa4aef109936ab390da6
Blake Saylor <blakesaylor@gmail.com>
2022-05-18 11:19:28 -0600

    Update to initialize rooms as empty array; passes test

--- lib/house.rb
+++ lib/house.rb
@@ -1,9 +1,10 @@
 class House
-  attr_reader :price, :address
+  attr_reader :price, :address, :rooms
 
   def initialize(price, address)
     @price = price.delete("$").to_i
     @address = address
+    @rooms = []
   end
 
 end
