dc79be74455595dfde0a1bb8859879c86b419c2c
Blake Saylor <blakesaylor@gmail.com>
2022-05-18 11:24:15 -0600

    Add test for above_market_average?

--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -32,5 +32,12 @@ RSpec.describe House do
       house.add_room(room_2)
       expect(house.rooms).to eq [room_1, room_2]
     end
+
+    it "is above market average if price > 500000" do
+      house_1 = House.new("$400000", "123 sugar lane")
+      house_2 = House.new("$575000", "1202 E 24th")
+      expect(house_1.above_market_average?).to eq false
+      expect(house_2.above_market_average?).to eq true
+    end
   end
 end
