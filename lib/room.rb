01b1c2d1e372af921de206ae7f81b5bb0dce80f8
Blake Saylor <blakesaylor@gmail.com>
2022-05-18 11:10:59 -0600

    Initialize class and add instance variables to pass test

--- lib/room.rb
+++ lib/room.rb
@@ -1,7 +1,10 @@
 class Room
+  attr_reader :category, :length, :width
 
-  def initialize
-
+  def initialize(category, length, width)
+    @category = category
+    @length = length
+    @width = width
   end
 
 
