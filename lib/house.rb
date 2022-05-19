cbc5ee81665e0d7a100c313c6b97fda1f8a232ac
Gauri Joshi <gauri728@gmail.com>
2022-05-18 21:07:53 -0700

    Iteration 2 Complete

--- lib/house.rb
+++ lib/house.rb
@@ -1,8 +1,14 @@
 class House
 attr_reader :price, :address, :rooms
+attr_accessor :rooms
   def initialize(price, address)
     @price = price
     @address = address
     @rooms = []
   end
+
+  def add_room(room_1)
+    @rooms << room_1
+  end
+
 end
--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -6,6 +6,10 @@ RSpec.describe House do
     it "exists" do
       house = House.new("$400000", "123 sugar lane")
       expect(house).to be_a(House)
+    end
+
+    it "has attributes" do
+      house = House.new("$400000", "123 sugar lane")
       expect(house.price).to eq("$400000")
       expect(house.address).to eq("123 sugar lane")
       expect(house.rooms).to eq([])
@@ -14,6 +18,16 @@ RSpec.describe House do
     it "has rooms" do
       room_1= Room.new(:bedroom, 10, '13')
       expect(room_1).to be_a(Room)
+    end
+
+    it "can have rooms added to itself" do
+      room_1= Room.new(:bedroom, 10, '13')
+      expect(room_1).to be_a(Room)
+      house = House.new("$400000", "123 sugar lane")
+      house.add_room(room_1)
+      room_2 = Room.new(:living_room, 15, '12')
+      house.add_room(room_2)
+      expect(house.rooms).to eq([room_1, room_2])
     end
   end
 end
