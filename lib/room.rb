0b9f1f1803d327ccb60cb6c3d2519c900e745661
Gauri Joshi <gauri728@gmail.com>
2022-05-18 20:04:22 -0700

    Test/it has an area, and Feat/define area

--- lib/room.rb
+++ lib/room.rb
@@ -1,6 +1,26 @@
 class Room
+attr_reader :category, :length, :width
+attr_accessor :width
+  def initialize(category, length, width)
+    @category = category
+    @length = length
+    @width = width
+    @paint = false
+  end
 
-  def initialize
+  def area
+    @length = length
+    @width = width.to_i
+    return @length * @width
+  end
+
+  def is_painted?
+    if @paint == true
+      false
+  else
+    true
+
+    end
 
   end
 
--- spec/room_spec.rb
+++ spec/room_spec.rb
@@ -4,21 +4,28 @@ RSpec.describe Room do
   describe "Iteration 1" do
     it "exists" do
       room = Room.new(:bedroom, 10, '13')
-      expect(room).to be_a Room
+      expect(room).to be_a(Room)
     end
 
-    xit "it has a category" do
+    it "has a category" do
       room = Room.new(:bedroom, 10, '13')
 
       expect(room.category).to eq(:bedroom)
     end
 
-    xit "can get area" do
-      room1 = Room.new(:bedroom, 10, '13')
-      room2 = Room.new(:living_room, 15, '12')
+    it "has an area" do
+      room_1= Room.new(:bedroom, 10, '13')
+      room_2 = Room.new(:living_room, 15, '12')
 
-      expect(room1.area).to eq(130)
-      expect(room2.area).to eq(180)
+      expect(room_1.area).to eq(130)
+      expect(room_2.area).to eq(180)
+    end
+
+    xit "can be painted" do
+      room = Room.new(:bedroom, 10, '13')
+      expect(room.is_painted?).to eq(false)
+      room.paint
+      expect(room.is_painted?).to eq(true)
     end
   end
 end
