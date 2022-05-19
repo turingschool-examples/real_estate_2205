881e3b8fb97e7ee90d195f72a0e40b96bfe8d8a6
Gauri Joshi <gauri728@gmail.com>
2022-05-18 20:59:00 -0700

    Test/it has rooms

--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -6,6 +6,14 @@ RSpec.describe House do
     it "exists" do
       house = House.new("$400000", "123 sugar lane")
       expect(house).to be_a(House)
+      expect(house.price).to eq("$400000")
+      expect(house.address).to eq("123 sugar lane")
+      expect(house.rooms).to eq([])
+    end
+
+    it "has rooms" do
+      room_1= Room.new(:bedroom, 10, '13')
+      expect(room_1).to be_a(Room)
     end
   end
 end
