87f5a7dc34b95ac76be2ce5dc1bf03c07dde6b80
Eli Sachs <easachs13@gmail.com>
2022-05-19 19:35:27 -0600

    Update House class (it 4)

--- lib/house.rb
+++ lib/house.rb
@@ -23,11 +23,16 @@ class House
   end
 
   def area
-    total_area = 0
-    @rooms.each do |room|
-      total_area += room.area
+    # total_area = 0
+    # @rooms.each do |room|
+    #   total_area += room.area
+    # end
+    # total_area
+
+    total_area = @rooms.map do |room|
+      room.area
     end
-    total_area
+    total_area.reduce(:+)
   end
 
   def details
@@ -35,6 +40,23 @@ class House
     details_hash["price"] = @price
     details_hash["address"] = @address
     details_hash
+  end
+
+  def price_per_square_foot
+    ppsf = @price.to_f / area
+    ppsf.round(2)
+  end
+
+  def rooms_sorted_by_area
+    @rooms.sort_by do |room|
+      room.area
+    end.reverse
+  end
+
+  def rooms_by_category
+    @rooms.group_by do |room|
+      room.category
+    end
   end
 
 end
