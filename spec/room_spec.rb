19ac86df758c444306e85520cdefd3cfac292f1d
Eli Sachs <easachs13@gmail.com>
2022-05-19 18:50:08 -0600

    Update Room spec

--- spec/room_spec.rb
+++ spec/room_spec.rb
@@ -7,18 +7,29 @@ RSpec.describe Room do
       expect(room).to be_a Room
     end
 
-    xit "it has a category" do
+    it "it has a category" do
       room = Room.new(:bedroom, 10, '13')
 
       expect(room.category).to eq(:bedroom)
     end
 
-    xit "can get area" do
+    it "can get area" do
       room1 = Room.new(:bedroom, 10, '13')
       room2 = Room.new(:living_room, 15, '12')
 
       expect(room1.area).to eq(130)
       expect(room2.area).to eq(180)
+    end
+
+    it "can check if is_painted?" do
+      room = Room.new(:bedroom, 10, '13')
+      expect(room.is_painted?).to be false
+    end
+
+    it "can be painted" do
+      room = Room.new(:bedroom, 10, '13')
+      room.paint
+      expect(room.is_painted?).to be true
     end
   end
 end
