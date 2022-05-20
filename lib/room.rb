6d1614f2091a99550ca6ecc8aa2a9c853b6f73fb
philmarcu <philmark18@gmail.com>
2022-05-19 18:27:33 -0600

    Finish iteration 1

--- lib/room.rb
+++ lib/room.rb
@@ -1,9 +1,14 @@
 class Room
 
   attr_reader :category, :width, :length
-  def initialize(:category, :width, :length)
-
+  def initialize(category, width, length)
+    @category = category
+    @width = width
+    @length = length
   end
 
+  def area
+    @width * @length.to_i
+  end
 
 end
