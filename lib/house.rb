b081e70d8b85ad243117d7050db4b5788f0e3242
Eli Sachs <easachs13@gmail.com>
2022-05-19 18:51:40 -0600

    Add House class (it 2 and 3)

---
+++ lib/house.rb
@@ -0,0 +1,40 @@
+class House
+
+  attr_reader :price, :address, :rooms
+
+  def initialize(price, address)
+    @price = price.delete("$").to_i
+    @address = address
+    @rooms = []
+  end
+
+  def add_room(room)
+    @rooms << room
+  end
+
+  def above_market_average?
+    @price > 500000
+  end
+
+  def rooms_from_category(category)
+    @rooms.find_all do |room|
+      room.category == category
+    end
+  end
+
+  def area
+    total_area = 0
+    @rooms.each do |room|
+      total_area += room.area
+    end
+    total_area
+  end
+
+  def details
+    details_hash = {}
+    details_hash["price"] = @price
+    details_hash["address"] = @address
+    details_hash
+  end
+
+end
