0df632dda95ddab65ac8447504a914cdf198d3ff
Eli Sachs <easachs13@gmail.com>
2022-05-19 18:50:28 -0600

    Add Room class

--- lib/room.rb
+++ lib/room.rb
@@ -1,8 +1,24 @@
 class Room
 
-  def initialize
+  attr_reader :category, :length, :width
 
+  def initialize(category, length, width)
+    @category = category
+    @length = length
+    @width = width
+    @is_painted = false
   end
 
+  def area
+    @length.to_i * @width.to_i
+  end
+
+  def is_painted?
+    @is_painted
+  end
+
+  def paint
+    @is_painted = true
+  end
 
 end
