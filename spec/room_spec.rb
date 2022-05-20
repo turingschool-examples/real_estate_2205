41994e7b26c72867961085d86743d0886cb6cf12
Eli Sachs <easachs13@gmail.com>
2022-05-19 19:36:19 -0600

    Clean Room spec

--- spec/room_spec.rb
+++ spec/room_spec.rb
@@ -1,7 +1,9 @@
 require './lib/room'
 
 RSpec.describe Room do
+
   describe "Iteration 1" do
+
     it "exists" do
       room = Room.new(:bedroom, 10, '13')
       expect(room).to be_a Room
@@ -9,14 +11,12 @@ RSpec.describe Room do
 
     it "it has a category" do
       room = Room.new(:bedroom, 10, '13')
-
       expect(room.category).to eq(:bedroom)
     end
 
-    it "can get area" do
+    it "can display area" do
       room1 = Room.new(:bedroom, 10, '13')
       room2 = Room.new(:living_room, 15, '12')
-
       expect(room1.area).to eq(130)
       expect(room2.area).to eq(180)
     end
@@ -31,5 +31,6 @@ RSpec.describe Room do
       room.paint
       expect(room.is_painted?).to be true
     end
+
   end
 end
