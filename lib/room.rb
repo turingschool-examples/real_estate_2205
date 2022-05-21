4f5413ecb883535514e423fb96f8eaee3b69f97a
alepbloyd <alexanderleeboyd@gmail.com>
2022-05-21 16:09:32 -0400

    Feat: Room initaliziation

--- lib/room.rb
+++ lib/room.rb
@@ -1,7 +1,11 @@
 class Room
 
-  def initialize
+  attr_reader :category, :length, :width
 
+  def initialize(category, length, width)
+    @category = category
+    @length = length
+    @width = width
   end
 
 
