23044484e6bc34fafe679314ecb917e490b77cef
alepbloyd <alexanderleeboyd@gmail.com>
2022-05-21 16:26:37 -0400

    Test: initializes with no rooms

--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -23,6 +23,12 @@ RSpec.describe House do
       expect(house.address).to eq("123 sugar lane")
     end
 
+    it 'initializes with no rooms' do
+      house = House.new("$400000", "123 sugar lane")
+
+      expect(house.rooms).to eq([])
+    end
+
   end
 
 end
