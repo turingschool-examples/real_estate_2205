680af4f2776444aaf57fc49dff16e58f12587d8c
Shirley DeCesari <shirley.decesari@gmail.com>
2022-05-20 19:41:32 -0700

    Add method for painted in room class

--- lib/room.rb
+++ lib/room.rb
@@ -3,8 +3,15 @@ class Room
               :area
   def initialize(category, length, width)
     @category = category
-    @area = length.to_i * width.to_i 
+    @area = length.to_i * width.to_i
+    @painted = false
   end
 
+  def is_painted?
+    @painted
+  end
 
+  def paint
+    @painted = true 
+  end
 end
