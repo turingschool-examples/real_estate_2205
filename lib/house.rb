e33cd604a49055672bc2c6a11e980ad19a5c70c8
Brenna Stuart <brennacodes@gmail.com>
2022-05-21 09:54:20 -0600

    Feat: Iteration 2 passing; house/house_spec

---
+++ lib/house.rb
@@ -0,0 +1,15 @@
+class House
+  attr_reader :price,
+              :address,
+              :rooms
+
+  def initialize(price, address)
+    @price = price.delete_prefix("$").to_i
+    @address = address
+    @rooms = []
+  end
+
+  def add_room(room)
+    @rooms << room
+  end
+end
--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -4,3 +4,30 @@ require './lib/house'
 RSpec.describe Room do
   let!(:room1) {Room.new(:bedroom, 10, '13')}
   let!(:room2) {Room.new(:living_room, 15, '12')}
+  let!(:house) {House.new("$400000", "123 sugar lane")}
+
+    describe "Iteration 2" do
+      it "is an instance of" do
+        expect(house).to be_instance_of(House)
+      end
+
+      it "has a price" do
+        expect(house.price).to eq(400000)
+      end
+
+      it "has an address" do
+        expect(house.address).to eq("123 sugar lane")
+      end
+
+      it "has rooms" do
+        expect(house.rooms).to eq([])
+      end
+
+      it "can add a room to itself" do
+        house.add_room(room1)
+        house.add_room(room2)
+        expect(house.rooms.length).to eq(2)
+        expect(house.rooms).to include(Room)
+      end
+    end
+  end
