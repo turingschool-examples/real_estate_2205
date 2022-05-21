fcc7fdc479ac9bca7ee97a6e860e4dace494e060
Bryce Simonds <bsimonds14@gmail.com>
2022-05-21 12:47:52 -0600

    test: test to calculate its price per square foot

--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -70,4 +70,18 @@ RSpec.describe House do
     expect(house.details).to eq({"price" => 400000, "address" => "123 sugar lane"})
   end
 
+  it "calculates its price per square foot" do
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    room_3 = Room.new(:living_room, 25, '15')
+    room_4 = Room.new(:basement, 30, '41')
+    house.add_room(room_4)
+    house.add_room(room_1)
+    house.add_room(room_3)
+    house.add_room(room_2)
+    expect(house.price_per_square_foot).to eq(210.53)
+  end
+
+
 end
