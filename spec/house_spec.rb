f94cd3d800ff9f23d0f4c1722ac359145c2486d9
Bryce Simonds <bsimonds14@gmail.com>
2022-05-21 12:43:45 -0600

    test: if house has hash of details

--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -57,4 +57,17 @@ RSpec.describe House do
     expect(house.area).to eq(1900)
   end
 
+  it "has a hash of house details" do
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    room_3 = Room.new(:living_room, 25, '15')
+    room_4 = Room.new(:basement, 30, '41')
+    house.add_room(room_1)
+    house.add_room(room_2)
+    house.add_room(room_3)
+    house.add_room(room_4)
+    expect(house.details).to eq({"price" => 400000, "address" => "123 sugar lane"})
+  end
+
 end
