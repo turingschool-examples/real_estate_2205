25d3981c68cc414e7b349d95cf27022cc70b156b
Nikky Rojas <rojas7313@gmail.com>
2022-05-21 14:53:47 -0600

    Initial commit

--- lib/room.rb
+++ lib/room.rb
@@ -1,8 +1,12 @@
 class Room
-
-  def initialize
-
-  end
+  attr_reader :category, :length, :width, :painted
+    def initialize (category, length, width)
+      @category = category
+      @length = length
+      @width = width
+      @area = []
+      @painted = []
+    end
 
 
 end
