575cf3411619e59976d4aa8a5f59049997bfcda8
Shirley DeCesari <shirley.decesari@gmail.com>
2022-05-20 19:30:29 -0700

    Initial commit

--- lib/room.rb
+++ lib/room.rb
@@ -1,7 +1,9 @@
 class Room
-
-  def initialize
-
+  attr_reader :category,
+              :area
+  def initialize(category, length, width)
+    @category = category
+    @area = length.to_i * width.to_i 
   end
 
 
