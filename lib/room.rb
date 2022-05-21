0ee7b4aa2ce6935392f142db88baa12bd73de6d6
mkbonini <mkbonini@gmail.com>
2022-05-21 12:10:39 -0600

    Add test for is_painted?

--- lib/room.rb
+++ lib/room.rb
@@ -1,7 +1,10 @@
 class Room
-
-  def initialize
-
+  attr_reader :category, :area
+  def initialize(category, length, width)
+    @category = category
+    @length = length
+    @width = width
+    @area = @length * @width.to_i
   end
 
 
