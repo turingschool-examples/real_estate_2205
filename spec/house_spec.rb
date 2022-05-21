a9161d8d17d7948569035239dee06851d035eb10
alepbloyd <alexanderleeboyd@gmail.com>
2022-05-21 16:46:28 -0400

    Test: returns total area

--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -68,6 +68,21 @@ RSpec.describe House do
       expect(house.rooms_from_category(:basement)).to eq([room_4])
     end
 
+    it 'returns total area' do
+      house = House.new("$400000", "123 sugar lane")
+      room_1 = Room.new(:bedroom, 10, '13')
+      room_2 = Room.new(:bedroom, 11, '15')
+      room_3 = Room.new(:living_room, 25, '15')
+      room_4 = Room.new(:basement, 30, '41')
+
+      house.add_room(room_1)
+      house.add_room(room_2)
+      house.add_room(room_3)
+      house.add_room(room_4)
+
+      expect(house.area).to eq(1900)
+    end
+
   end
 
 end
