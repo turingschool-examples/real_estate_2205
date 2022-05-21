b46615a4596c81486800cb7124f606cb56e37ca5
Bryce Simonds <bsimonds14@gmail.com>
2022-05-21 12:51:34 -0600

    test: returns list of rooms sorted by area

--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -83,5 +83,18 @@ RSpec.describe House do
     expect(house.price_per_square_foot).to eq(210.53)
   end
 
+  it "returns a list of rooms sorted by area" do
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    room_3 = Room.new(:living_room, 25, '15')
+    room_4 = Room.new(:basement, 30, '41')
+    house.add_room(room_4)
+    house.add_room(room_1)
+    house.add_room(room_3)
+    house.add_room(room_2)
+    expect(house.rooms_sorted_by_area).to eq([room_4, room_3, room_2, room_1])
+  end
+
 
 end
