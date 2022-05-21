0fa4e660a549687c62615dee50062d1422b13b1e
mkbonini <mkbonini@gmail.com>
2022-05-21 12:47:30 -0600

    Test: house - rooms by category

--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -37,13 +37,29 @@ RSpec.describe House do
 
       expect(house.rooms).to eq([room_1, room_2])
     end
-
-    it 'can calculate abouve market average of $500,000' do
+  end
+  describe "Iteration 3" do
+    it 'can calculate above market average of $500,000' do
         house_1 = House.new("$400000", "123 sugar lane")
         house_2 = House.new("$600000", "123 sugar lane")
 
         expect(house_1.above_market_average?).to be false
         expect(house_2.above_market_average?).to be true
+    end
+
+    it 'can return rooms by category' do
+      house = House.new("$400000", "123 sugar lane")
+      room_1 = Room.new(:bedroom, 10, '13')
+      room_2 = Room.new(:bedroom, 11, '15')
+      room_3 = Room.new(:living_room, 25, '15')
+      room_4 = Room.new(:basement, 30, '41')
+
+      house.add_room(room_1)
+      house.add_room(room_2)
+      house.add_room(room_3)
+      house.add_room(room_4)
+
+      expect(house.rooms_from_category(:bedroom)).to eq([room_1, room_2])
     end
   end
 end
