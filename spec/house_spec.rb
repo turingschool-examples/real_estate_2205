355e723c6609502b48295a55dc33e75b867e5dc1
alepbloyd <alexanderleeboyd@gmail.com>
2022-05-21 16:40:41 -0400

    Test: rooms_from_category

--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -52,6 +52,21 @@ RSpec.describe House do
       expect(house.above_market_average?).to be true
     end
 
+    it 'returns rooms from category' do
+      house = House.new("$400000", "123 sugar lane")
+      room_1 = Room.new(:bedroom, 10, '13')
+      room_2 = Room.new(:bedroom, 11, '15')
+      room_3 = Room.new(:living_room, 25, '15')
+      room_4 = Room.new(:basement, 30, '41')
+
+      house.add_room(room_1)
+      house.add_room(room_2)
+      house.add_room(room_3)
+      house.add_room(room_4)
+
+      expect(house.rooms_from_category(:bedroom)).to eq([room_1,room_2])
+    end
+
   end
 
 end
