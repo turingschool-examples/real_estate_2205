ae2b60ba86b68f3e249b765b9d5cdc5166162186
mkbonini <mkbonini@gmail.com>
2022-05-21 12:25:08 -0600

    Feature: house has rooms array

--- lib/house.rb
+++ lib/house.rb
@@ -1,10 +1,11 @@
 class House
 
-  attr_reader :price, :address
+  attr_reader :price, :address, :rooms
 
   def initialize(price, address)
     @price = price
     @address = address
+    @rooms = []
   end
 
 end
