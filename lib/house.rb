14e5dc72b9a6e3a26960ba19a0fef1a2f356f7d8
Nikky Rojas <rojas7313@gmail.com>
2022-05-21 15:00:31 -0600

    add house class with add_rooms method

---
+++ lib/house.rb
@@ -0,0 +1,13 @@
+class House
+  attr_reader :price, :address, :rooms
+  def initialize(price, address)
+    @price = price
+    @address = address
+    @rooms = []
+  end
+
+  def add_rooms(the_room)
+    @rooms << the_room
+  end
+
+end
--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -6,3 +6,15 @@ RSpec.describe House do
     house = House.new("$400000", "123 sugar lane")
     expect(house).to be_a(House)
   end
+
+  it "has rooms" do
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    house.add_rooms(room_1)
+    house.add_rooms(room_1)
+
+    expect(house.add_rooms(room_1)).to eq (house.rooms)
+
+  end
+end
