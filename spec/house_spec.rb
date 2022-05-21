fae255f576195d2f8b9926fc4c971cdaea5ec106
Bryce Simonds <bsimonds14@gmail.com>
2022-05-21 11:48:57 -0600

    test: house can have additional rooms

--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -14,5 +14,13 @@ RSpec.describe House do
     expect(house.rooms).to eq([])
   end
 
+  it "it can have additional rooms" do
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    house.add_room(room_1)
+    house.add_room(room_2)
+    expect(house.rooms).to eq([room_1, room_2])
+  end
 
 end
