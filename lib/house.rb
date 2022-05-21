56d8aec3844a7b946c30fe0b2feea4da023e6b0f
alepbloyd <alexanderleeboyd@gmail.com>
2022-05-21 16:27:13 -0400

    Feat: initializes with no rooms

--- lib/house.rb
+++ lib/house.rb
@@ -1,10 +1,11 @@
 class House
 
-  attr_reader :price, :address
+  attr_reader :price, :address, :rooms
 
   def initialize(price,address)
     @price = price[1..-1].to_i
     @address = address
+    @rooms = []
   end
 
 end
