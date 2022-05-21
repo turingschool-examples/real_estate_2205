7c9e4d7ce73e90432c4498159fbcd564b8790878
Bryce Simonds <bsimonds14@gmail.com>
2022-05-21 12:15:06 -0600

    feature: lists rooms by category

--- lib/house.rb
+++ lib/house.rb
@@ -17,4 +17,18 @@ class House
       false
     end
   end
+
+  def rooms_from_category(room_type)
+    rooms_to_return = []
+    rooms_not_to_return = []
+
+    @rooms.each do |room|
+      if room.category == room_type
+        rooms_to_return << room
+      elsif
+      rooms_not_to_return << room
+      end
+    end
+    rooms_to_return
+  end
 end
