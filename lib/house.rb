7f3ed9e98ca0f62597297aea3838f16d012fb017
Shirley DeCesari <shirley.decesari@gmail.com>
2022-05-20 20:11:17 -0700

    Complete iteration 3

--- lib/house.rb
+++ lib/house.rb
@@ -11,4 +11,23 @@ class House
   def add_room(room)
     @rooms << room
   end
+
+  def above_market_average?
+    @price > 5000000
+  end
+
+  def rooms_from_category(category)
+    @rooms.find_all {|room| room.category == category}
+  end
+
+  def area
+    @rooms.sum {|room| room.area}
+  end
+
+  def details
+    {
+      "price" => @price,
+      "address" => @address
+    }
+  end
 end
--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -6,6 +6,8 @@ RSpec.describe House do
   let(:house) {House.new("$400000", "123 sugar lane")}
   let(:room_1) {Room.new(:bedroom, 10, '13')}
   let(:room_2) {Room.new(:bedroom, 11, '15')}
+  let(:room_3) {Room.new(:living_room, 25, '15')}
+  let(:room_4) {Room.new(:basement, 30, '41')}
 
   it "exists and has attributes" do
     expect(house.price).to eq 400000
@@ -18,5 +20,42 @@ RSpec.describe House do
     house.add_room(room_2)
 
     expect(house.rooms).to eq [room_1, room_2]
+  end
+
+  it "#above_market_average" do
+    expect(house.above_market_average?).to eq false
+  end
+
+  it "#rooms_from_category" do
+    house.add_room(room_1)
+    house.add_room(room_2)
+    house.add_room(room_3)
+    house.add_room(room_4)
+
+    expect(house.rooms_from_category(:bedroom)).to eq [room_1, room_2]
+    expect(house.rooms_from_category(:basement)).to eq [room_4]
+  end
+
+  it "#area" do
+    house.add_room(room_1)
+    house.add_room(room_2)
+    house.add_room(room_3)
+    house.add_room(room_4)
+
+    expect(house.area).to eq 1900
+  end
+
+  it "#details" do
+    house.add_room(room_1)
+    house.add_room(room_2)
+    house.add_room(room_3)
+    house.add_room(room_4)
+
+    expected = {
+      "price" => 400000,
+      "address" => "123 sugar lane"
+    }
+
+    expect(house.details).to eq expected
   end
 end
