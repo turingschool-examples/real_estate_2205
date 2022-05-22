3830d252b82fc2b8f6705c9488b16ca9fe31da9d
John Santosuosso <john.santosuosso1@gmail.com>
2022-05-22 18:33:03 -0400

    initial commit

--- lib/room.rb
+++ lib/room.rb
@@ -1,8 +1,22 @@
 class Room
+  attr_reader :category, :length, :width, :painted
 
-  def initialize
-
+  def initialize(category, length, width)
+    @category = category
+    @length = length
+    @width = width
   end
 
+  def area
+    @length * @width.to_i
+  end
+
+  def is_painted?
+    @painted = false
+  end
+
+  def paint
+    @painted = true
+  end
 
 end
