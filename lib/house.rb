a4c558e2457da4cb753f3b91e6d27a838ebbc775
John Santosuosso <john.santosuosso1@gmail.com>
2022-05-22 19:38:34 -0400

    update house and house spec so all tests pass

--- lib/house.rb
+++ lib/house.rb
@@ -1,15 +1,40 @@
 require_relative 'room'
 
 class House
-  attr_reader :price, :address, :rooms
+  attr_reader :price, :address, :rooms, :house_details
   def initialize(price, address)
     @price = price
     @address = address
     @rooms = []
+    @house_details = {}
   end
 
   def add_room(room)
     @rooms << room
   end
 
+  def above_market_average?
+    @price.slice!(0)
+    @price.to_i > 500000
+  end
+
+  def rooms_from_category(category)
+    @rooms.select {|room| room.category == category}
+  end
+
+  def area
+    area_house = []
+    @rooms.each {|room| area_house << room.area}
+    area_house.sum
+  end
+
+  def details
+    @price.slice!(0)
+    @house_details["price"] = @price.to_i
+    @house_details["address"]= @address
+    @house_details
+  end
+
 end
+
+require 'pry'; binding.pry
--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -32,5 +32,40 @@ RSpec.describe House do
       expect(house.rooms.count).to eql(2)
     end
 
+    it 'returns true if home price is above market average' do
+      house = House.new("$400000", "123 sugar lane")
+      house1 = House.new("$600000", "123 mulberry lane")
+      expect(house.above_market_average?).to be false
+      expect(house1.above_market_average?).to be true
+    end
+
+    it 'selects rooms from a category' do
+      house = House.new("$400000", "123 sugar lane")
+      room_1 = Room.new(:bedroom, 10, '13')
+      room_2 = Room.new(:bedroom, 11, '15')
+      room_3 = Room.new(:living_room, 25, '15')
+      room_4 = Room.new(:basement, 30, '41')
+      house.add_room(room_1)
+      house.add_room(room_2)
+      house.add_room(room_3)
+      house.add_room(room_4)
+      expect(house.rooms_from_category(:bedroom).count).to eql(2)
+      expect(house.rooms_from_category(:basement).count).to eql(1)
+
+    end
+
+    it 'finds the area of all rooms in a house' do
+      house = House.new("$400000", "123 sugar lane")
+      room_1 = Room.new(:bedroom, 10, '13')
+      room_2 = Room.new(:bedroom, 10, '15')
+      house.add_room(room_1)
+      house.add_room(room_2)
+      expect(house.area).to eql(280)
+    end
+
+    it 'returns the details of the house' do
+      house = House.new("$400000", "123 sugar lane")
+      expect(house.details).to eql({"price" => 400000, "address" => "123 sugar lane"})
+    end
   end
 end
