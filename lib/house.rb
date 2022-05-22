135b004442e4f430c4d5e7a04b46eb137ab6193b
Wyatt Kribs <kribswyatt@gmail.com>
2022-05-22 17:56:10 -0600

    final commit

--- lib/house.rb
+++ lib/house.rb
@@ -1,1 +1,71 @@
-#start
+class House
+  attr_reader :rooms
+  def initialize(price, address)
+    @price = price
+    @address = address
+    @rooms = []
+  end
+
+  def add_room(room)
+    @rooms << room
+  end
+
+  def above_market_average?
+    if @price.to_i > 500000
+      true
+    else
+      false
+    end
+  end
+
+  def rooms_from_category(type)
+    rooms_of_cat = []
+    rooms.each do |room|
+      if room.category == type
+        rooms_of_cat << room
+      end
+    end
+    rooms_of_cat
+  end
+
+  def area
+    area = 0
+    rooms.each do |room|
+      area += room.area
+    end
+    area
+  end
+
+  def details
+    details = {}
+    details["price"] = @price.delete_prefix("$").to_i
+    details["address"] = @address
+    p details
+  end
+
+  def price_per_square_foot
+    price = details["price"].to_f
+    sq_foot = area.to_f
+    ppsf = (price / sq_foot).round(2)
+    p ppsf
+  end
+
+  #unsure where to start with this method.
+  def rooms_sorted_by_area
+    big_to_small = []
+    rooms.each do |room|
+    end
+    big_to_small
+  end
+
+  def rooms_by_category
+    room_hash = {}
+    room_array = []
+    rooms.each do |room|
+      if room_hash.key?(room.category)
+        room_hash[room.category] = room_array << room
+      end
+    end
+    room_hash
+  end
+end
---
+++ spec/house_spec.rb
@@ -0,0 +1,103 @@
+require './lib/room'
+require './lib/house'
+
+RSpec.describe House do
+  describe "Iteration 2" do
+
+    it "is an instance of house" do
+      house = House.new("$400000", "123 sugar lane")
+      expect(house).to be_a(House)
+    end
+
+    it "can add rooms" do
+      house = House.new("$400000", "123 sugar lane")
+      room_1 = Room.new(:bedroom, 10, '13')
+      room_2 = Room.new(:bedroom, 11, '15')
+      house.add_room(room_1)
+      house.add_room(room_2)
+      expect(house.rooms).to eq([room_1, room_2])
+    end
+  end
+
+  describe "Iteration 3" do
+
+    it "isn't above market average by default" do
+      house = House.new("$400000", "123 sugar lane")
+      expect(house.above_market_average?).to eq(false)
+    end
+
+    it "can tell you how many rooms there are by category" do
+      house = House.new("$400000", "123 sugar lane")
+      room_1 = Room.new(:bedroom, 10, '13')
+      room_2 = Room.new(:bedroom, 11, '15')
+      room_3 = Room.new(:living_room, 25, '15')
+      room_4 = Room.new(:basement, 30, '41')
+      house.add_room(room_1)
+      house.add_room(room_2)
+      house.add_room(room_3)
+      house.add_room(room_4)
+      # require 'pry' ; binding.pry
+      expect(house.rooms_from_category(:bedroom)).to eq([room_1, room_2])
+      expect(house.rooms_from_category(:basement)).to eq([room_4])
+    end
+
+    it "can tell you the total house area" do
+      house = House.new("$400000", "123 sugar lane")
+      room_1 = Room.new(:bedroom, 10, '13')
+      room_2 = Room.new(:bedroom, 11, '15')
+      room_3 = Room.new(:living_room, 25, '15')
+      room_4 = Room.new(:basement, 30, '41')
+      house.add_room(room_1)
+      house.add_room(room_2)
+      house.add_room(room_3)
+      house.add_room(room_4)
+      expect(house.area).to eq(1900)
+    end
+
+    it "can store the details" do
+      house = House.new("$400000", "123 sugar lane")
+      expect(house.details).to eq({"price" => 400000, "address" => "123 sugar lane"})
+    end
+  end
+
+  describe "Iteration 4" do
+    it "can calculate price per square foot" do
+      house = House.new("$400000", "123 sugar lane")
+      room_1 = Room.new(:bedroom, 10, '13')
+      room_2 = Room.new(:bedroom, 11, '15')
+      room_3 = Room.new(:living_room, 25, '15')
+      room_4 = Room.new(:basement, 30, '41')
+      house.add_room(room_1)
+      house.add_room(room_2)
+      house.add_room(room_3)
+      house.add_room(room_4)
+      expect(house.price_per_square_foot).to eq(210.53)
+    end
+
+    xit "can sort rooms by greatest area" do
+      house = House.new("$400000", "123 sugar lane")
+      room_1 = Room.new(:bedroom, 10, '13')
+      room_2 = Room.new(:bedroom, 11, '15')
+      room_3 = Room.new(:living_room, 25, '15')
+      room_4 = Room.new(:basement, 30, '41')
+      house.add_room(room_1)
+      house.add_room(room_2)
+      house.add_room(room_3)
+      house.add_room(room_4)
+      expect(house.rooms_sorted_by_area).to eq([room_4, room_3, room_2, room_1])
+    end
+
+    it "can sort the rooms by category" do
+      house = House.new("$400000", "123 sugar lane")
+      room_1 = Room.new(:bedroom, 10, '13')
+      room_2 = Room.new(:bedroom, 11, '15')
+      room_3 = Room.new(:living_room, 25, '15')
+      room_4 = Room.new(:basement, 30, '41')
+      house.add_room(room_1)
+      house.add_room(room_2)
+      house.add_room(room_3)
+      house.add_room(room_4)
+      expect(house.rooms_by_category).to eq({:bedroom => [room_1, room_2], :living_room => [room_3], :basement => [room_4]})
+    end
+  end
+end
