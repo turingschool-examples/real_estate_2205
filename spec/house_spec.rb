2e43e93f49c426d8dcf357f62ba26aa47c1ca979
Lucas Colwell <colwellc37@gmail.com>
2022-05-23 08:22:59 -0600

    Test: write basic tests for house class

---
+++ spec/house_spec.rb
@@ -0,0 +1,21 @@
+require 'rspec'
+require './lib/room'
+require './lib/house'
+
+RSpec.describe House do
+  describe 'Iteration 1' do
+    before :each do
+      @house = House.new("$400000", "123 sugar lane")
+    end
+
+    it 'exists' do
+      expect(@house).to be_a(House)
+    end
+
+    it 'has attributes' do
+      expect(@house.price).to eq(400000)
+      expect(@house.address).to eq("123 sugar lane")
+      expect(@house.rooms).to eq([])
+    end
+
+  end
