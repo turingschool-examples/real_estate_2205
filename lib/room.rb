8e4cfec6dddf84713d5cd20664d09533e95d9d76
Lucas Colwell <colwellc37@gmail.com>
2022-05-23 07:59:29 -0600

    Feat: initialize category and area

--- lib/room.rb
+++ lib/room.rb
@@ -1,8 +1,12 @@
 class Room
+  attr_reader :category,
+              :area
 
-  def initialize
-
+  def initialize(category, length_in_ft, width_in_ft)
+    @category = category
+    @area = length_in_ft * width_in_ft.to_i
   end
+
 
 
 end
