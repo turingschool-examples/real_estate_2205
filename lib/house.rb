3647830c7376e28e2a74c32ac32b1fd7cc19af7e
Thiago Silveira <thiago1992@yahoo.com>
2022-05-23 07:55:24 -0600

    Feat: Pass test house checks if its above market average

--- lib/house.rb
+++ lib/house.rb
@@ -4,9 +4,18 @@ class House
     @price = price[1..-1].to_i
     @address = address
     @rooms = []
+    @above_market_avg = true
   end
 
   def add_room(room)
     @rooms << room
+  end
+
+  def above_market_average?
+    if @price > 500000
+      @above_market_avg
+    elsif @price <= 500000
+      !@above_market_avg
+    end
   end
 end
