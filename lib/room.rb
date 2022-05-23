e2f6dca62aadee2ca42aa58f585d85f8830e3aa5
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-05-23 16:05:21 -0600

    FEAT: Completed default tests and methods

--- lib/room.rb
+++ lib/room.rb
@@ -1,10 +1,14 @@
 class Room
   attr_reader :category, :length, :width
+
   def initialize(category, length, width)
     @category = category
     @length = length
     @width = width
+  end
 
+  def area
+    total_area = @length * @width.to_i
   end
 
 
