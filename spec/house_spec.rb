3f038454e3751d8426a5de267004497c47d28be5
Kevin Ta <kvnta@yahoo.com>
2022-05-23 17:31:52 -0600

    Add exists and attributes test to house_spec

--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -7,5 +7,24 @@ Rspec.describe Room do
       house = House.new("$400000", "123 sugar lane")
       expect(house).to be_a House
     end
+    it "has a price" do
+      house = House.new("$400000", "123 sugar lane")
+      expect(house.price).to eq(400000)
+    end
 
-    
+    it "has an address" do
+      house = House.new("$400000", "123 sugar lane")
+      expect(house.address).to eq("123 sugar lane")
+    end
+    it "has rooms" do
+      house = House.new("$400000", "123 sugar lane")
+      expect(house.rooms).to eq([])
+    end
+
+    end
+    end
+
+    end
+
+  end
+end
