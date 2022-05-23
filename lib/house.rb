810aa1a37ffe4e26eb4f1307fcc1d33c7a6da72a
Gwendolyn Ruiz <gwen_marina@yahoo.com>
2022-05-22 19:48:02 -0700

    Feat/added code for above_market_aveage? method

--- lib/house.rb
+++ lib/house.rb
@@ -9,5 +9,15 @@ class House
     @rooms = []
   end
 
+  def add_room(room)
+    @rooms << room
+  end
 
+  def above_market_average?
+    if @price > 500000
+      true
+    else
+      false
+    end
+  end
 end
