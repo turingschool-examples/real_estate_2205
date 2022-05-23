fae33473cb223b465c3fb7965079a29590c1874d
Gwendolyn Ruiz <gwen_marina@yahoo.com>
2022-05-22 19:42:04 -0700

    Test/added test for add_room method

--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -2,6 +2,8 @@ require './lib/house'
 
 RSpec.describe House do
   let(:house) { House.new("$400000", "123 sugar lane") }
+  let(:room_1) { Room.new(:bedroom, 10, '13') }
+  let(:room_2) { Room.new(:bedroom, 11, '15') }
 
   it "exists" do
     expect(house).to be_a(House)
@@ -13,4 +15,10 @@ RSpec.describe House do
     expect(house.rooms).to eq([])
   end
 
+  it "can check that the house has rooms" do
+    house.add_room(room_1)
+    house.add_room(room_2)
+
+    expect(house.rooms).to eq([room_1, room_2])
+  end
 end
