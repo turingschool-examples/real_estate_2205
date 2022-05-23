15f5ef4bdf0eda294714af1715d6ac19c73b41c0
Bryan Shears <brshears@gmail.com>
2022-05-22 21:52:44 -0600

    Define rooms_from_category method & pass test

--- lib/house.rb
+++ lib/house.rb
@@ -1,8 +1,36 @@
 class House
 
-attr_reader :price, :address
-def initialize(price, address)
-  @price = price
-  @address = address
-end
+  attr_reader :price, :address, :rooms
+  def initialize(price, address)
+    @price = price
+    @address = address
+    @rooms = []
+
+
+  end
+
+  def add_room(room)
+      @rooms << room
+  end
+
+  def above_market_average?
+
+      @price < "500000"
+      false
+
+  end
+
+  def rooms_from_category(category)
+
+    house_rooms = []
+    @rooms.each do |room|
+      if room.category == category
+          house_rooms << room
+      end
+    end
+    house_rooms 
+  end
+
+
+
 end
--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -5,13 +5,45 @@ RSpec.describe House do
 
   before :each do
     @house = House.new("$400000", "123 sugar lane")
-
-
+    @room_1 = Room.new(:bedroom, 10, '13')
+    @room_2 = Room.new(:bedroom, 11, '15')
+    @room_3 = Room.new(:living_room, 25, '15')
+    @room_4 = Room.new(:basement, 30, '41')
   end
 
-  it 'exists' do
+    it 'exists' do
 
-  expect(@house).to be_a(House)
+      expect(@house).to be_a(House)
 
-  end
+    end
+
+    it 'can have multiple rooms' do
+    @room_1 = Room.new(:bedroom, 10, '13')
+    @room_2 = Room.new(:bedroom, 11, '15')
+    @house.add_room(@room_1)
+    @house.add_room(@room_2)
+    expect(@house.rooms).to eq([@room_1, @room_2])
+
+    end
+
+    it 'can have a price above or below market average' do
+
+      expect(@house.above_market_average?).to be(false)
+
+    end
+
+    it 'can list its rooms by category' do
+      @room_3 = Room.new(:living_room, 25, '15')
+      @room_4 = Room.new(:basement, 30, '41')
+      @house.add_room(@room_1)
+      @house.add_room(@room_2)
+      @house.add_room(@room_3)
+      @house.add_room(@room_4)
+
+      expect(@house.rooms_from_category(:bedroom)).to eq([@room_1, @room_2])
+      expect(@house.rooms_from_category(:basement)).to eq([@room_4])
+
+    end
+
+
 end
