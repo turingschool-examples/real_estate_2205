63dd5b2028f4a316b11d9a57c40991c343740516
Drew MacNicholas <dmacnicholas@gmail.com>
2022-05-22 22:07:11 -0500

    Add has a price test

--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -3,10 +3,17 @@ require './lib/house'
 
 RSpec.describe House do
   describe "Iteration 2" do
-    it ' exists' do
+    it "exists" do
       house = House.new("$400000", "123 sugar lane")
 
       expect(house).to be_a(House)
     end
+
+    it "has a price" do
+      house = House.new("$400000", "123 sugar lane")
+
+      expect(house.price).to eq(400000)
+
+    end
   end
-end  
+end
