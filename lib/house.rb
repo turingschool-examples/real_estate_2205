3f7d17d680d552fe9b7360fd81faeb7a636a2cf1
Ethan Nguyen <etnguyen16@gmail.com>
2022-05-22 20:36:53 -0600

    add tests that can pass

--- lib/house.rb
+++ lib/house.rb
@@ -13,12 +13,47 @@ class House
     @rooms << room
   end
 
+  #I am stuck here. I keep getting an error that says expected: "400000" got: 400000
+  # I fixed it...but I am unsure how. All i did was type, delete, and then retype. Maybe this was a 'save' issue?
   def above_market_average?
-  if @price.delete("$").to_i > 500000
-    true
+    if @price.delete("$").to_i > 500000
+      true
+    else
+      false
+    end
   end
-    false
+
+
+  def rooms_from_category(category)
+    @rooms.find_all do |room|
+      room.category == category
+    end
   end
+
+
+  def area
+    area = 0
+    rooms.each do |room|
+      area += room.area
+    end
+    return area
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -15,13 +15,13 @@ RSpec.describe House do
 
     it 'has a price' do
       house = House.new("$400000", "123 sugar lane")
-      expect(house.price).to eq("400000")
+      expect(house.price).to eq("$400000")
 
     end
 
 
 
-    xit 'has an address' do
+    it 'has an address' do
       house = House.new("$400000", "123 sugar lane")
       expect(house.address).to eq("123 sugar lane")
 
@@ -29,7 +29,7 @@ RSpec.describe House do
 
 
 
-    xit 'has no rooms' do
+    it 'has no rooms' do
       house = House.new("$400000", "123 sugar lane")
       expect(house.rooms).to eq([])
 
@@ -37,25 +37,67 @@ RSpec.describe House do
 
 
 
-    xit 'adds rooms' do
+    it 'adds rooms' do
       house = House.new("$400000", "123 sugar lane")
       room_1 = Room.new(:bedroom, 10, '13')
       room_2 = Room.new(:bedroom, 11, '15')
       house.add_room(room_1)
       house.add_room(room_2)
-      expect(house.new). to eq([room_1, room_2])
+      expect(house).to eq([room_1, room_2])
+
     end
+
   end
 
 
 
+    #I missed reading this step in the iteration.
+  describe "above market price" do
+    it 'calculates market average higher than 500000' do
+      house = House.new("$400000", "123 sugar lane")
+      expect(house.above_market_average?).to be false
+    end
 
 
 
+    it 'returns rooms by category' do
+      house = House.new("$400000", "123 sugar lane")
+      room_1 = Room.new(:bedroom, 10, '13')
+      room_2 = Room.new(:bedroom, 11, '15')
+      room_3 = Room.new(:living_room, 25, '15')
+      room_4 = Room.new(:basement, 30, '41')
+      house.add_room(room_1)
+      house.add_room(room_2)
+      house.add_room(room_3)
+      house.add_room(room_4)
+      expect(house.rooms_from_category(:bedroom)).to eq([room_1, room_2])
+
+    end
 
 
 
+    it 'can add the room area' do
+      house = House.new("$400000", "123 sugar lane")
+      room_1 = Room.new(:bedroom, 10, '13')
+      room_2 = Room.new(:bedroom, 11, '15')
+      room_3 = Room.new(:living_room, 25, '15')
+      room_4 = Room.new(:basement, 30, '41')
+      house.add_room(room_1)
+      house.add_room(room_2)
+      house.add_room(room_3)
+      house.add_room(room_4)
+      expect(house.area).to eq(1900)
 
+    end
+
+
+
+    xit 'uses hash' do
+      house = House.new("$400000", "123 sugar lane")
+      expect(house.details).to eq({"price" => 400000, "address" => "123 sugar lane"})
+
+    end
+  end
 
 
 end
