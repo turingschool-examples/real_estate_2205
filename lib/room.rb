bb4dc9a4c7c209cd706d8899ea63b91cffdc5206
Jared Hardinger <jaredhardinger@gmail.com>
2022-05-22 23:21:44 -0400

    Createnew methods for iteration 1

--- lib/room.rb
+++ lib/room.rb
@@ -1,8 +1,23 @@
 class Room
+  attr_reader :category
 
-  def initialize
-
+  def initialize(category, length, width)
+    @category = category
+    @length = length
+    @width = width
+    @painted = false
   end
 
+  def area
+    @length * @width.to_i
+  end
+
+  def paint
+    @painted = true
+  end
+
+  def is_painted?
+    @painted
+  end
 
 end
