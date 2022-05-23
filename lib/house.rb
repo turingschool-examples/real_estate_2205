ede4d27dfe18f94b569c7479bd46daf491bf22ae
Jared Hardinger <jaredhardinger@gmail.com>
2022-05-23 00:33:38 -0400

    Add methods to finish iteration 3

--- lib/house.rb
+++ lib/house.rb
@@ -5,9 +5,26 @@ class House
     @price = price.delete("$").to_i
     @address = address
     @rooms = []
+    @details = {}
   end
 
   def add_room(room)
     @rooms << room
+  end
+
+  def above_market_average?
+    @price > 500000
+  end
+
+  def rooms_from_category(room_category)
+    @rooms.find_all {|room| room.category == room_category}
+  end
+
+  def area
+    @rooms.sum {|room| room.area}
+  end
+
+  def details
+    details = {"price" => @price, "address" => @address}
   end
 end
