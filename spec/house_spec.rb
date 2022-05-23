7f190ff3acb57099e11a5c8c21feeb58b31badde
Thiago Silveira <thiago1992@yahoo.com>
2022-05-23 07:40:16 -0600

    Test: House can add different rooms

--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -13,4 +13,13 @@ RSpec.describe House do
     expect(house.address).to eq("123 sugar lane")
     expect(house.rooms).to eq([])
   end
+
+  it 'can add different rooms' do
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    house.add_room(room_1)
+    house.add_room(room_2)
+    expect(house.rooms).to eq(room_1, room_2)
+  end
 end
