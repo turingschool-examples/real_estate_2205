bb9f31349796ee97127c5c2bc27d19ac49870c67
Thiago Silveira <thiago1992@yahoo.com>
2022-05-23 08:41:04 -0600

    Test: House can return rooms by category hash

--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -92,4 +92,23 @@ RSpec.describe House do
     expect(house.rooms_sorted_by_area).to eq([room_4, room_3, room_2, room_1])
     #rooms are listed from greatest area to least area.
   end
+
+  it 'can return room by category hash' do
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    room_3 = Room.new(:living_room, 25, '15')
+    room_4 = Room.new(:basement, 30, '41')
+    house.add_room(room_4)
+    house.add_room(room_3)
+    house.add_room(room_2)
+    house.add_room(room_1)
+    expected = {
+      :bedroom => [room_1, room_2],
+      :living_room => [room_3],
+      :basement => [room_4],
+    }
+    expect(house.rooms_by_category).to eq(expected)
+    # returns a hash where the keys are symbols of each room category, and the values are a collection of room instances that match the category
+  end
 end
