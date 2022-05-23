3aaae31e92fa1e84cfd0ffb33ef6033994cee4a5
Jared Hardinger <jaredhardinger@gmail.com>
2022-05-22 23:54:10 -0400

    Create remaining tests

--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -1,3 +1,4 @@
+require './lib/room'
 require './lib/house'
 
 RSpec.describe House do
@@ -5,6 +6,25 @@ RSpec.describe House do
     it "exists" do
       house = House.new("$400000", "123 sugar lane")
       expect(house).to be_a House
+    end
+
+    it "has a price" do
+      house = House.new("$400000", "123 sugar lane")
+      expect(house.price).to eq(400000)
+    end
+
+    it "has an address" do
+      house = House.new("$400000", "123 sugar lane")
+      expect(house.address).to eq("123 sugar lane")
+    end
+
+    it "has rooms in a house" do
+      house = House.new("$400000", "123 sugar lane")
+      room_1 = Room.new(:bedroom, 10, '13')
+      room_2 = Room.new(:bedroom, 11, '15')
+      house.add_room(room_1)
+      house.add_room(room_2)
+      expect(house.rooms).to eq([room_1, room_2])
     end
   end
 end
