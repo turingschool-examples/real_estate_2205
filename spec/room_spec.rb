348562e343e0a10924ab6fe6602d515b208b50a8
Lucas Colwell <colwellc37@gmail.com>
2022-05-23 08:05:59 -0600

    Test: write test checking painted status

--- spec/room_spec.rb
+++ spec/room_spec.rb
@@ -2,23 +2,34 @@ require './lib/room'
 
 RSpec.describe Room do
   describe "Iteration 1" do
+    before :each do
+      @room = Room.new(:bedroom, 10, '13')
+      @room1 = @room
+      @room2 = Room.new(:living_room, 15, '12')
+    end
+
     it "exists" do
-      room = Room.new(:bedroom, 10, '13')
-      expect(room).to be_a Room
+      expect(@room).to be_a Room
     end
 
     it "it has a category" do
-      room = Room.new(:bedroom, 10, '13')
-
-      expect(room.category).to eq(:bedroom)
+      expect(@room.category).to eq(:bedroom)
     end
 
     it "can get area" do
-      room1 = Room.new(:bedroom, 10, '13')
-      room2 = Room.new(:living_room, 15, '12')
-
-      expect(room1.area).to eq(130)
-      expect(room2.area).to eq(180)
+      expect(@room1.area).to eq(130)
+      expect(@room2.area).to eq(180)
     end
+
+    it 'can tell if room is painted, and can change status' do
+      expect(@room1.painted?).to be(false)
+      expect(@room2.painted?).to be(false)
+
+      @room1.paint
+
+      expect(@room1.painted?).to be(true)
+      expect(@room2.painted?).to be(false)
+    end
+
   end
 end
