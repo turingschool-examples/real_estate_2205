50f6a64a379b6186d17fc8f4c6cc7f5c4eea37fa
Thiago Silveira <thiago1992@yahoo.com>
2022-05-23 07:34:34 -0600

    Test: House has attributes

--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -6,4 +6,11 @@ RSpec.describe House do
     house = House.new("$400000", "123 sugar lane")
     expect(house).to be_a(House)
   end
+
+  it 'has attributes' do
+    house = House.new("$400000", "123 sugar lane")
+    expect(house.price).to eq(400000)
+    expect(house.address).to eq("123 sugar lane")
+    expect(house.rooms).to eq([])
+  end
 end
