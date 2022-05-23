d06c9db96c5fd37205e1e0294f47fe0ae1adea57
Lucas Colwell <colwellc37@gmail.com>
2022-05-23 08:32:14 -0600

    Test: write add_room test

--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -3,9 +3,11 @@ require './lib/room'
 require './lib/house'
 
 RSpec.describe House do
-  describe 'Iteration 1' do
+  describe 'Iteration 2' do
     before :each do
       @house = House.new("$400000", "123 sugar lane")
+      @room_1 = Room.new(:bedroom, 10, '13')
+      @room_2 = Room.new(:bedroom, 11, '15')
     end
 
     it 'exists' do
@@ -18,6 +20,11 @@ RSpec.describe House do
       expect(@house.rooms).to eq([])
     end
 
-  end
+    it 'can add rooms to house' do
+      @house.add_room(@room_1)
+      @house.add_room(@room_2)
 
+      expect(@house.rooms).to eq([@room1, @room2])
+    end
+  end
 end
