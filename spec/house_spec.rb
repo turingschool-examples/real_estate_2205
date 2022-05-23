316f7d532ad511ab11e47ba49c8eacc3b85d7db8
Lucas Colwell <colwellc37@gmail.com>
2022-05-23 08:55:13 -0600

    Test: create above mkt value and category tests

--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe House do
       @house = House.new("$400000", "123 sugar lane")
       @room_1 = Room.new(:bedroom, 10, '13')
       @room_2 = Room.new(:bedroom, 11, '15')
-      @room_3 = Room.new(:basement, 20, '15')
+      @room_x = Room.new(:basement, 20, '15')
     end
 
     it 'exists' do
@@ -27,9 +27,32 @@ RSpec.describe House do
 
       expect(@house.rooms).to eq([@room_1, @room_2])
             #robust test
-      @house.add_room(@room_3)
+      @house.add_room(@room_x)
 
-      expect(@house.rooms).to eq([@room_1, @room_2, @room_3])
+      expect(@house.rooms).to eq([@room_1, @room_2, @room_x])
+    end
+
+    describe 'iteration 3' do
+      before :each do
+        @room_3 = Room.new(:living_room, 25, '15')
+        @room_4 = Room.new(:basement, 30, '41')
+        @house_x = House.new("$510000", "42 street 0")
+      end
+
+      it 'can tell if house is above mkt avg' do
+        expect(@house.above_market_average?).to be(false)
+           #robust test
+        expect(@house_x.above_market_average?).to be(true)
+      end
+
+      it 'can tell rooms from a given category' do
+        @house.add_room(@room_1)
+        @house.add_room(@room_2)
+        @house.add_room(@room_3)
+        @house.add_room(@room_4)
+
+        expect(@house.rooms_from_category(:bedroom)).to eq([@room_1, @room_2, @room_3, @room_4])
+      end
     end
   end
 end
