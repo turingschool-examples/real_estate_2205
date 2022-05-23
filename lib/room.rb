1f74f608675b165bf87f9672d9b90fa7d1aedf98
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-05-23 16:13:50 -0600

    FEAT: Completed methods and passed all iteration 1 tests

--- lib/room.rb
+++ lib/room.rb
@@ -1,15 +1,22 @@
 class Room
-  attr_reader :category, :length, :width
+  attr_reader :category, :length, :width, :paint_status
 
   def initialize(category, length, width)
     @category = category
     @length = length
     @width = width
+    @paint_status = false
   end
 
   def area
     total_area = @length * @width.to_i
   end
 
+  def is_painted?
+    @paint_status
+  end
 
+  def paint
+    @paint_status = true
+  end
 end
