0f33131c6c25e159bc145664b075d03cda0b1080
Thiago Silveira <thiago1992@yahoo.com>
2022-05-23 07:13:43 -0600

    Feat: Pass test room returns area calculation

--- lib/room.rb
+++ lib/room.rb
@@ -1,8 +1,12 @@
 class Room
-  attr_reader :category
+  attr_reader :category, :length, :width
   def initialize(category, length, width)
     @category = category
+    @length = length
+    @width = width.to_i
   end
 
-
+  def area
+    @length * @width
+  end
 end
