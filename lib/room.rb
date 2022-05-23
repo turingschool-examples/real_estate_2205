0b5517a2f43ab834040f6b9a8022f7b34d5671f1
Gwendolyn Ruiz <gwen_marina@yahoo.com>
2022-05-22 19:23:51 -0700

    Feat/added code to initialize method and attr_reader for category test to pass

--- lib/room.rb
+++ lib/room.rb
@@ -1,9 +1,13 @@
 class Room
-  
+  attr_reader :category,
+              :length,
+              :width
+
 
   def initialize(category, length, width)
-
-
+    @category = category
+    @length = length
+    @width = width
   end
 
 
