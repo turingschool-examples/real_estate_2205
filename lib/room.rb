a6414254169f1c6f2d888a5461f5543c87e4a9fa
Drew MacNicholas <dmacnicholas@gmail.com>
2022-05-22 21:35:20 -0500

    Add initialize method

--- lib/room.rb
+++ lib/room.rb
@@ -1,6 +1,11 @@
 class Room
 
-  def initialize
+  attr_reader :category, :length, :width
+
+  def initialize(category, length, width)
+    @category = category
+    @lenth = length
+    @width = width
 
   end
 
