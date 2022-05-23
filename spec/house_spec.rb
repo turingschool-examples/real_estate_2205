03e09f1706c692c68608c177cedab0d8c709e7b6
Gwendolyn Ruiz <gwen_marina@yahoo.com>
2022-05-22 19:51:43 -0700

    Test/added test for rooms by category

--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -5,6 +5,8 @@ RSpec.describe House do
   let(:house) { House.new("$400000", "123 sugar lane") }
   let(:room_1) { Room.new(:bedroom, 10, '13') }
   let(:room_2) { Room.new(:bedroom, 11, '15') }
+  let(:room_3) { Room.new(:living_room, 25, '15') }
+  let(:room_4) { Room.new(:basement, 30, '41') }
 
   it "exists" do
     expect(house).to be_a(House)
@@ -25,5 +27,15 @@ RSpec.describe House do
 
   it "can tell if a house is above market average" do
     expect(house.above_market_average?).to eq false
+  end
+
+  it "can list all rooms in the house by category" do
+    house.add_room(room_1)
+    house.add_room(room_2)
+    house.add_room(room_3)
+    house.add_room(room_4)
+
+    expect(house.rooms_from_category(:bedroom)).to eq([room_1, room_2])
+    expect(house.rooms_from_category(:basement)).to eq([room_4])
   end
 end
