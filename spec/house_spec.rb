84ae4f0c496015ec2c64a1d3324b564e3d5c2b0b
Thiago Silveira <thiago1992@yahoo.com>
2022-05-23 07:59:01 -0600

    Test: House can list its rooms by category

--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -27,4 +27,19 @@ RSpec.describe House do
     house = House.new("$400000", "123 sugar lane")
     expect(house.above_market_average?).to eq(false)
   end
+
+  it 'can list its rooms by category' do
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    room_3 = Room.new(:living_room, 25, '15')
+    room_4 = Room.new(:basement, 30, '41')
+    house.add_room(room_1)
+    house.add_room(room_2)
+    house.add_room(room_3)
+    house.add_room(room_4)
+    expect(house.rooms_from_category(:bedroom)).to eq([room_1, room_2])
+    expect(house.rooms_from_category(:basement)).to eq([room_4])
+    expect(house.rooms_from_category(:living_room)).to eq([room_3])
+  end
 end
