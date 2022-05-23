32017a98edd025cb14ad890d72de509f306fc2cc
Thiago Silveira <thiago1992@yahoo.com>
2022-05-23 07:31:53 -0600

    Test: House exists

--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -2,5 +2,8 @@ require './lib/room'
 require './lib/house'
 
 RSpec.describe House do
-
+  it 'exists' do
+    house = House.new("$400000", "123 sugar lane")
+    expect(house).to be_a(House)
+  end
 end
