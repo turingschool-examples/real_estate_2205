9863931da38d0c84caf084a5ce2d99bfcf0d0a30
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-05-23 16:03:41 -0600

    initial commit

--- lib/room.rb
+++ lib/room.rb
@@ -1,6 +1,9 @@
 class Room
-
-  def initialize
+  attr_reader :category, :length, :width
+  def initialize(category, length, width)
+    @category = category
+    @length = length
+    @width = width
 
   end
 
