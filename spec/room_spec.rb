74a697c4d08cdba870f648d11df0de7f765db913
Jared Hardinger <jaredhardinger@gmail.com>
2022-05-22 23:53:08 -0400

    Refactor

--- spec/room_spec.rb
+++ spec/room_spec.rb
@@ -9,14 +9,12 @@ RSpec.describe Room do
 
     it "it has a category" do
       room = Room.new(:bedroom, 10, '13')
-
       expect(room.category).to eq(:bedroom)
     end
 
     it "can get area" do
       room1 = Room.new(:bedroom, 10, '13')
       room2 = Room.new(:living_room, 15, '12')
-
       expect(room1.area).to eq(130)
       expect(room2.area).to eq(180)
     end
@@ -24,17 +22,14 @@ RSpec.describe Room do
     it "is painted" do
       room1 = Room.new(:bedroom, 10, '13')
       room2 = Room.new(:living_room, 15, '12')
-
       expect(room1.is_painted?).to be false
       expect(room2.is_painted?).to be false
 
       room1.paint
-
       expect(room1.is_painted?).to be true
       expect(room2.is_painted?).to be false
 
       room2.paint
-
       expect(room1.is_painted?).to be true
       expect(room2.is_painted?).to be true
     end
